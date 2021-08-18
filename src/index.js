dd1bb5b328507a0c016fd313eccfac84d2858df0
Oleg Bloschinskyi <carrentkharkov@gmail.com>
2021-08-18 16:01:38 +0300

    18.08.21

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import colorCardsTpl from './templates/cards.hbs';
+import cardsData from './menu.json';
 
+const galleryRef = document.querySelector('.js-menu');
+const markup= colorCardsTpl(cardsData);
+galleryRef.insertAdjacentHTML('beforeend', markup);
